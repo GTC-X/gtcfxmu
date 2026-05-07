f83671754a25f8c656eacd9a4cef06026fd617ab
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2026-05-07 16:11:04 +0400

    account type heading

--- app/[locale]/components/home/TradingPlatform.jsx
+++ app/[locale]/components/home/TradingPlatform.jsx
@@ -3,7 +3,6 @@ import Link from "next-intl/link";
 import Image from "next/image";
 
 export default function TradingPlatform() {
-
   const t = useTranslations("home.accountType");
 
   const accountTypes = [
@@ -25,8 +24,6 @@ export default function TradingPlatform() {
         <div className="flex flex-col lg:flex-row items-center justify-between gap-5 md:gap-10">
           {/* Left Section - Asset Markets */}
           <div className="text-center md:text-left basis-full md:basis-2/4 border-b-2 md:border-none border-gray-200">
-      
-           
             <div className="flex flex-row items-center justify-center md:justify-start gap-3">
               <div className="relative w-32 md:w-full h-28 md:h-80">
                 <Image
@@ -39,25 +36,24 @@ export default function TradingPlatform() {
                 />
               </div>
               <div className="md:w-[750px] ltr:md:ml-[-150px] rtl:md:mr-[-150px]">
-              <h2 className="HeadingH2 md:text-4xl font-bold italic">
-            {t("assetNum")} <span className=" font-normal  md:text-3xl">{t("assetTit")}</span>
-        </h2>
-              <h2 className="HeadingH2 lg:text-[50px] md:my-6 text-secondary font-medium rtl:md:text-right ltr:md:text-left italic">
-              {t("assetMar")}
-        </h2>
-            
-                <p className="text-xs md:text-sm rtl:md:text-right ltr:md:text-left"> {t("AssetNam")} </p>
+                <h2 className="HeadingH2 md:text-4xl font-bold italic">
+                  {t("assetNum")} <span className=" font-normal  md:text-3xl">{t("assetTit")}</span>
+                </h2>
+                <h2 className="HeadingH2 lg:text-[50px] md:my-6 text-secondary font-medium rtl:md:text-right ltr:md:text-left italic">
+                  {t("assetMar")}
+                </h2>
+                <p className="text-xs md:text-sm rtl:md:text-right ltr:md:text-left">{t("AssetNam")}</p>
               </div>
             </div>
           </div>
 
           {/* Right Section - Account Types */}
           <div className="text-center basis-full md:basis-2/4">
-           {/* Section Title */}
-        <h2 className="HeadingH2 md:mb-5 font-medium">
-          {t("heading")}
-        </h2>
-        <p className="text max-w-xl mx-auto mb-5">{t("desc")}</p>
+            {/* Section Title */}
+            <h2 className="HeadingH2 md:mb-5 font-medium">
+              {t("heading")}
+            </h2>
+            <p className="text max-w-xl mx-auto mb-5">{t("desc")}</p>
 
             {/* Account Type Boxes with Background Images */}
             <div className="flex flex-row justify-center items-center gap-4 md:gap-4 md:mt-6">
@@ -71,8 +67,7 @@ export default function TradingPlatform() {
                     backgroundPosition: "center",
                   }}
                 >
-                
-               
+                  <h3 className="text-primary md:w-44 font-bold text-base">{account.heading}</h3> {/* Rendering the heading */}
                 </div>
               ))}
             </div>
